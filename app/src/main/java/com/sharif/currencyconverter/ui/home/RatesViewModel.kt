f2bd44999fb03a87ccdbf8d3d7b9b88e63bc84bb
androidcodehunter <sharif.iit.du@gmail.com>
2019-11-12 08:40:31 +0600

    initial repository setup.

--- app/src/main/java/com/sharif/currencyconverter/ui/home/RatesViewModel.kt
+++ app/src/main/java/com/sharif/currencyconverter/ui/home/RatesViewModel.kt
@@ -2,14 +2,16 @@ package com.sharif.currencyconverter.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.sharif.currencyconverter.data.source.RatesRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class RatesViewModel(private val ratesRepository: RatesRepository) {
+class RatesViewModel(private val ratesRepository: RatesRepository): ViewModel() {
 
     fun getRates(){
         viewModelScope.launch(Dispatchers.IO){
-            ratesRepository.getRates("")
+            ratesRepository.getRates("EUR")
         }
 
     }
