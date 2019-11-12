8a1fb6836689637b67ae549e2454557d43ca2f3a
androidcodehunter <sharif.iit.du@gmail.com>
2019-11-12 08:41:06 +0600

    initial repository setup.

--- app/src/main/java/com/sharif/currencyconverter/data/source/remote/RatesRemoteDataSource.kt
+++ app/src/main/java/com/sharif/currencyconverter/data/source/remote/RatesRemoteDataSource.kt
@@ -6,6 +6,7 @@ import com.sharif.currencyconverter.data.source.RatesDataSource
 import com.sharif.currencyconverter.data.source.Result
 
 class RatesRemoteDataSource(private val converterApiService: ConverterApiService): RatesDataSource {
+    
     override suspend fun getRates(base: String): Result<RateList> {
         return Result.Success(converterApiService.getTopTracks(base))
     }
