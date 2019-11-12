e6e9d2aed7b04f04ddcfe5a0a3dd09209dc7d84e
androidcodehunter <sharif.iit.du@gmail.com>
2019-11-12 19:32:28 +0600

    initial repository setup.

---
+++ app/src/main/java/com/sharif/currencyconverter/ui/adapter/CurrencyRatesAdapter.kt
@@ -0,0 +1,68 @@
+package com.sharif.currencyconverter.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.sharif.currencyconverter.R
+import com.sharif.currencyconverter.data.model.Rate
+import kotlinx.android.synthetic.main.list_item_currency_converter.view.*
+
+class CurrencyRatesAdapter: ListAdapter<Rate, CurrencyRatesAdapter.RateViewHolder>(CURRENCY_RATES_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RateViewHolder {
+        return RateViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item_currency_converter, parent, false))
+    }
+
+    override fun onBindViewHolder(
+        holder: RateViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        if (payloads.isEmpty()){
+            super.onBindViewHolder(holder, position, payloads)
+        } else{
+            holder.bindTo(getItem(position))
+        }
+    }
+
+    override fun onBindViewHolder(holder: RateViewHolder, position: Int) {
+        holder.bindTo(getItem(position))
+    }
+
+    inner class RateViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private val ivCurrencySymbol = itemView.ivCurrencySymbol
+        private val tvCurrencySymbol = itemView.tvCurrencySymbol
+        private val tvCurrencyName = itemView.tvCurrencyName
+        private val etCurrencyAmount = itemView.etCurrencyAmount
+
+        fun bindTo(rate: Rate) {
+            tvCurrencySymbol.text = rate.symbol
+            tvCurrencyName.text = "European"
+            etCurrencyAmount.setText(rate.rate.toString())
+        }
+
+    }
+
+    companion object{
+
+        /**
+         * Currency comparator to check for new data updates only, it will ignore duplicate data update.
+         * This technique is very effective when you update data continuously.
+         */
+        private val CURRENCY_RATES_COMPARATOR = object : DiffUtil.ItemCallback<Rate>() {
+            override fun areItemsTheSame(oldItem: Rate, newItem: Rate): Boolean {
+                return oldItem.symbol == newItem.symbol
+            }
+
+            override fun areContentsTheSame(oldItem: Rate, newItem: Rate): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
