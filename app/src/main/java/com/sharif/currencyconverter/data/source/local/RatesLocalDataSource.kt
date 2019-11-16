f2364aa65b5a91fe73886517ec0198847e2e55da
androidcodehunter <sharif.iit.du@gmail.com>
2019-11-16 12:43:15 +0600

    add local db.

--- app/src/main/java/com/sharif/currencyconverter/data/source/local/RatesLocalDataSource.kt
+++ app/src/main/java/com/sharif/currencyconverter/data/source/local/RatesLocalDataSource.kt
@@ -8,6 +8,10 @@ import java.lang.Exception
 class RatesLocalDataSource: RatesDataSource {
 
     override suspend fun getRates(base: String): Result<RateList> {
+
+        
+
+
         return Result.Error(Exception("Not implemented yet!!"))
     }
 
