bb6e6e942cbeab7d0b6a325f692d90ef68cf4de5
androidcodehunter <sharif.iit.du@gmail.com>
2019-11-17 08:32:18 +0600

    refactor local data.

---
+++ app/src/test/java/com/sharif/currencyconverter/ui/home/RatesConverterViewModelTest.kt
@@ -0,0 +1,11 @@
+package com.sharif.currencyconverter.ui.home
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class RatesConverterViewModelTest{
+    @Before
+    fun setUpViewModel(){
+
+    }
+}
